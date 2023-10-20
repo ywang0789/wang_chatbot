ced5a1fc8e2e12e780997ba61e74bea76fab8980
wang <ywang0789@conestogac.on.ca>
2023-10-20 18:15:45 -0400

    testing api

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print ("hello world")
+import openai 
+
+KEY = ""
+
+openai.api_key = KEY
+
+MODEL = "gpt-3.5-turbo"
+
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "user", "content": "Knock knock."}
+    ]
+)
+
+print(response['choices'][0]['message']['content'])
+
+
+
