3abf3232324976e51642c0cb14442868724972a6
wang <ywang0789@conestogac.on.ca>
2023-10-21 19:44:58 -0400

    added new basic UI and not using pyuic

--- main.py
+++ main.py
@@ -1,7 +1,70 @@
-from gui import *
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+
+import api_keys as keys
+
+import bot
+
+
+
+
+
+# class for app
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        loadUi("gui.ui", self)
+
+        self.home_button.clicked.connect(self.showHome)
+        self.chat_button.clicked.connect(self.showChat)
+        self.settings_button.clicked.connect(self.showSettings)
+
+    # functions for home button
+    def showHome(self):
+        self.stackedWidget.setCurrentIndex(0)
+
+    # functions for chat button
+    def showChat(self):
+        self.stackedWidget.setCurrentIndex(1)
+    
+    # functions for settings button
+    def showSettings(self):
+        self.stackedWidget.setCurrentIndex(2)
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myApp = ChatbotGUI()
-    myApp.show()
-    sys.exit(app.exec_())
+    ui = UI()
+    ui.show()
+
+    
+    app.exec_()
+
+    
+    
+
+
+
+# if __name__ == "__main__":
+#     app = QtWidgets.QApplication(sys.argv)
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_MainWindow()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
+
+# clicked events
+
+
+        # def showHome(self):
+        #         self.stackedWidget.setCurrentIndex(0)
+
+        # def showChat(self):
+        #         self.stackedWidget.setCurrentIndex(1)
+
+
+
+
+
+
+#     sys.exit(app.exec_())
